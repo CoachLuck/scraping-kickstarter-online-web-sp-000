67b67f637d8135f8803071246860f138d76d27e5
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:45:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,6 +18,10 @@ def get_page
   Nokogiri::HTML(open("fixtures/kickstarter.html"))
 end
 
+def get_projects
+  get_page.css(".project-card")
+end
+
 def create_project_hash
   # write your code here
 end

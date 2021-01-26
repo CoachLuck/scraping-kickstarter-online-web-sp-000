450d326b1bdbb853b2486ba34cf3c9ef9c2fef28
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:49:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,36 +1,40 @@
 require "nokogiri"
 
-class Project
-  attr_accessor :name, :link, :description, :location, :percent_funded
-
-  @@all = {}
-
-  def initialize
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-end
-
-def get_page
-  Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
-end
-
-def get_projects
-  get_page.css(".project-card")
-end
-
-def create_projects
-  get_projects.each {|proj|
-    project = Project.new
-    project.name = proj.css("bbcard_name").children[0].text
-    project.
-    project.description = proj.css("bbcard_blurb").text
-  }
-end
+# class Project
+#   attr_accessor :name, :link, :description, :location, :percent_funded
+# 
+#   @@all = {}
+# 
+#   def initialize
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# end
+# 
+# def get_page
+#   Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
+# end
+# 
+# def get_projects
+#   get_page.css(".project-card")
+# end
+# 
+# def create_projects
+#   get_projects.each {|proj|
+#     project = Project.new
+#     project.name = proj.css("bbcard_name").children[0].text
+#     project.
+#     project.description = proj.css("bbcard_blurb").text
+#   }
+# end
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash

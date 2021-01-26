6a503ffe9adaf33fbc9584b6c74387a95e4978f7
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 19:17:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -42,7 +42,6 @@ def create_project_hash
   projects = {}
   get_projects.each {|project|
     name = project.css("h2.bbcard_name strong a").text
-    projects[name] = {}
     projects[name] = {
       image_link: project.css("div.project-thumbnail a img").attribute("src").value,
       description: project.css("p.bbcard_blurb").text,

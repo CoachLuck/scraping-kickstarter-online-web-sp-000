79d4ede4f0c4d40eb7f4662faeb9a799977a41df
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:37:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,21 @@
 require_relative "fixtures/kickstarter.html"
+require "nokogiri"
+
+class Project 
+  attr_accessor :name, :link, :description, :location, :percent_funded
+
+  @@all 
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+end
+
+scraper = 
 
 def create_project_hash
   # write your code here

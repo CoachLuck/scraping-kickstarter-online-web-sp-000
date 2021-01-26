25058e9c4ebc980eefd337b3b50af68333bbd309
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:49:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,26 +2,26 @@ require "nokogiri"
 
 # class Project
 #   attr_accessor :name, :link, :description, :location, :percent_funded
-# 
+#
 #   @@all = {}
-# 
+#
 #   def initialize
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
 # end
-# 
+#
 # def get_page
 #   Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
 # end
-# 
+#
 # def get_projects
 #   get_page.css(".project-card")
 # end
-# 
+#
 # def create_projects
 #   get_projects.each {|proj|
 #     project = Project.new

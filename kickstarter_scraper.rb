13807a2b1a05564747788ec5450089d207be1953
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 19:09:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -32,9 +32,24 @@ require "pry"
 #   }
 # end
 
-def create_project_hash
+def get_projects(html)
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  kickstarter.css("li.project.grid_4")
+end
+
+def create_project_hash
+  hash = {}
+  get_projects.each {|project|
+    name = project.css("h2.bbcard_name strong a").text
+    hash[name] = {
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("ul.project-meta li a span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text
+    }
+  }
+
   binding.pry
   # projects: kickstarter.css("li.project.grid_4").first
   # name: project.css("h2.bbcard_name strong a").text
